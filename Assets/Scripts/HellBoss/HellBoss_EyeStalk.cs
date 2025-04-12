aeb1251a3fc48b48e22b5ef88a6f97f0fa66cb59
Logan Torr <113713716+loginTorr@users.noreply.github.com>
2025-04-12 16:55:46 -0500

    HellBoss Rotation, and Spawn

--- Assets/Scripts/HellBoss/HellBoss_EyeStalk.cs
+++ Assets/Scripts/HellBoss/HellBoss_EyeStalk.cs
@@ -5,23 +5,40 @@ using UnityEngine;
 public class HellBoss_EyeStalk : MonoBehaviour
 {
     public float health = 500f;
+    public float followSpeed;
+
+    public Transform PlayerPos;
+    public Transform EyePos;
+
+    public GameObject eye;
+
 
     public static HellBoss_EyeStalk HellBossInstance { get; private set; }
 
     // Start is called before the first frame update
     void Start()
     {
-        HellBossInstance = this;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        var step = followSpeed * Time.deltaTime;
+        Quaternion rot = transform.rotation;
+
+        transform.LookAt(PlayerPos.transform);
+        transform.rotation = Quaternion.Lerp(rot, transform.rotation, step);
+
+
         if (health <= 0)
         {
             Destroy(gameObject);
         }
+
+        //Ray ray = new Ray()
     }
+
 
     public void EyeHit()
     {
