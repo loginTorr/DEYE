e92fbc443e33e94a9ca6716c4ae15bd4ac745c26
Cheoddox <leeroywycoff@gmail.com>
2025-04-11 20:02:32 -0500

    Added Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2f;
+    public Transform cam;
+    public float mouseSensitivity = 100f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private bool DoubleJumpReady;
+    private float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handleMouse();
+        handleMovement();
+    }
+
+    void handleMouse()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cam.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    void handleMovement()
+    {
+        isGrounded = controller.isGrounded;
+        if (isGrounded)
+            DoubleJumpReady = true;
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical"); 
+
+        Vector3 move = (cam.forward * v + cam.right * h).normalized;
+        move.y = 0;
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isGrounded && DoubleJumpReady)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            DoubleJumpReady = false;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
